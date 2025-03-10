33b4339bd8f9efd4c287b85a49baa173d54d1b31
DylanDirschnabel <dyland@furb.br>
2025-03-10 12:39:19 -0300

    Alterações User-Role

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,9 +1,7 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 import jakarta.persistence.*;
 
@@ -24,7 +22,13 @@ public class User {
 	
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
-	
+
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+				joinColumns = @JoinColumn(name = "user_id"),
+				inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
 	public User() {}
 	
 	public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
@@ -86,6 +90,23 @@ public class User {
 	
 	public List<Order> getOrders() {
 		return orders;	
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+
+	public boolean hasRole(String roleName) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
